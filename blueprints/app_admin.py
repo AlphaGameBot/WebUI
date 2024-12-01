7c2ae5f62d02ca3f5bb40655ae08ba4e4b7dd07d
Damien Boisvert <damien@alphagame.dev>
2024-11-30 20:48:39 -0800

    FINALLY! We can now get one guild, and not have to get all of them lol

--- blueprints/app_admin.py
+++ blueprints/app_admin.py
@@ -5,6 +5,8 @@ from utility import (
     get_user_guilds,
     has_permission,
     get_guild_by_id,
+    get_user_guild_by_id,
+    user_has_administrator
 )
 
 app_admin = Blueprint("app_admin", __name__)
@@ -12,9 +14,11 @@ app_admin = Blueprint("app_admin", __name__)
 def button2int(b):
     return 1 if b == "on" else 0
 
+def userHasPermissionToServer(guild):
+    return has_permission(guild["permissions"], 8)
+
 @app_admin.route("/")
 def app_admin_index():
-    print("app_admin")
     token = request.cookies.get("access_token")
     user = get_user_info(token)
     cursor = cnx.cursor()
@@ -38,6 +42,8 @@ def app_admin_index():
 def app_guild(guildid):
     token = request.cookies.get("access_token")
     user = get_user_info(token)
+    if not user_has_administrator(token, guildid):
+        return render_template("simple-message.html", title="No Permission", message="You do not have permission to do this. (Nice try, though!)", user=user)
     cursor = cnx.cursor()
     cursor.execute("SELECT * FROM guild_settings WHERE guildid=%s", (guildid,))
     guilddb = cursor.fetchone()
@@ -49,26 +55,6 @@ def app_guild(guildid):
     if guild.get("code") == 10004:
         return render_template("simple-message.html", user=user, title="Guild Not Found", message="AlphaGameBot can't find the requested guild.  Either it doesn't exist (it happens to the best of us!), or AlphaGameBot is not in that guild, yet.")
     return render_template("app/admin_guild.html", user=user, guild=guild, guilddb=guilddb, leveling=leveling)
-
-def app_admin_wtf():
-    token = request.cookies.get("access_token")
-    user = get_user_info(token)
-    cursor = cnx.cursor()
-    cursor.execute("SELECT guildid FROM guild_settings")
-    agb_guilds_tp = cursor.fetchall()
-    agb_guilds = []
-    for i in agb_guilds_tp:
-        agb_guilds.append(i[0])
-    admin_guilds = []
-    guilds = get_user_guilds(token)
-    for guild in guilds:
-        gid = int(guild["id"])
-        
-        if gid not in agb_guilds: continue
-        if has_permission(guild["permissions"], 8):
-            admin_guilds.append(guild)
-        
-    return render_template("app/admin.html", user=user, guilds=admin_guilds)
 
 @app_admin.route("/guild/<int:guildid>/updateSettings", methods=["POST"])
 def app_guild_update(guildid):
--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
-from requests import get
+from requests import get, post
+from requests import __version__ as requests_version
 from json import loads
 from os import getenv
 import asyncio, aiohttp
@@ -13,33 +14,63 @@ DB_CONNECTION_INFO = {
 }
 cnx = mysql.connector.connect(**DB_CONNECTION_INFO)
 
+def makeRequestHeaders(token = None, bot = False):
+    headers = {
+        "User-Agent": "AlphaGameBot-WebUI (https://alphagamebot.alphagame.dev); curl/8.3.0; python-requests/%s" % requests_version,
+        "Authorization": "None NoToken"
+    }
+    if bot:
+        headers["Authorization"] = "Bot %s" % getenv("BOT_TOKEN")
+    else:
+        headers["Authorization"] = "Bearer %s" % token
+    return headers
+
 def seperatedNumberByComma(number):
     return "{:,}".format(number)
-def has_permission(permissions, permission):
+
+def has_permission(permissions, permission) -> bool:
     return permissions & permission != 0
 
 def get_user_by_id(user_id):
-    r = get("https://discord.com/api/users/%s" % user_id, headers={
-        "Authorization": "Bot %s" % getenv("BOT_TOKEN")
-    })
-    return loads(r.content)
+    r = get("https://discord.com/api/users/%s" % user_id, headers=makeRequestHeaders(bot = True))
+    return r.json()
 
 def get_user_info(access_token: str) -> dict:
-    r = get("https://discord.com/api/users/@me", headers={
-        "Authorization": "Bearer %s" % access_token
-    })
-    return loads(r.content)
+    r = get("https://discord.com/api/users/@me", headers=makeRequestHeaders(access_token))
+    return r.json()
 
 def get_user_guilds(access_token):
-        url = "https://discord.com/api/users/@me/guilds"
-        headers = {
-            "Authorization": "Bearer %s" % access_token
-        }
-        response = get(url, headers=headers)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return []
+    url = "https://discord.com/api/users/@me/guilds"
+    response = get(url, headers=makeRequestHeaders(access_token))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        current_app.logger.error("Failed to get user guilds: %s: %s" % (response.status_code, response.json()))
+        return []
+
+def get_user_guild_by_id(access_token, guild_id):
+    url = f"https://discord.com/api/users/@me/guilds/{guild_id}/member"
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    response = get(url, headers=headers)
+    return response.json()
+
+def get_guild_roles(guild_id):
+    url = "https://discord.com/api/guilds/%s/roles" % guild_id
+    return get(url, headers=makeRequestHeaders(bot = True)).json()
+
+def user_has_administrator(token, guildid):
+    guild = get_user_guild_by_id(token, guildid)
+    roles = get_guild_roles(guildid)
+    for role in roles:
+        if role not in guild["roles"]:
+            print("does not have role", role)
+            continue
+        if role["permissions"] & 8 == 8:
+            return True
+    return False
 
 def get_guild_by_id(guild_id):
     url = "https://discord.com/api/guilds/%s" % guild_id
